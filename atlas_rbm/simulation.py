904eb8a7250cfb41e5d4b74d5d864ca3bffc1d9a
Rodrigo <glucksfall@users.noreply.github.com>
2020-05-02 03:35:21 -0400

    fully functional API for metabolism

--- atlas_rbm/simulation.py
+++ atlas_rbm/simulation.py
@@ -12,19 +12,40 @@ __license__ = 'gpl-3.0'
 from pysb import *
 from pysb.bng import generate_network, generate_equations
 from pysb.pathfinder import set_path
-from pysb.simulator import ScipyOdeSimulator, BngSimulator
+from pysb.simulator import ScipyOdeSimulator, BngSimulator, KappaSimulator
+from pysb.util import alias_model_components
 from pylab import linspace
 
 import pandas
 import seaborn
 import matplotlib.pyplot as plt
 
-def set_initial(model, name, loc, new_value):
+def set_parameter(model, name, new_value):
 	for i in model.parameters._elements:
-		if 't0_' + name + '_' + loc in i.name:
+		if name == i.name:
 			i.value = new_value
-
 	return model
+
+class set_initial:
+	def monomers(model, name, loc = 'cyt', new_value = 0):
+		for i in model.parameters._elements:
+			if 't0_' + name + '_' + loc.lower() == i.name:
+				i.value = new_value
+		return model
+
+	def met(model, name, loc = 'cyt', new_value = 0):
+		return set_initial.monomers(model, name, loc, new_value)
+
+	def cplx(model, name, loc = 'cyt', new_value = 0):
+		return set_initial.monomers(model, name, loc, new_value)
+
+	def prot(model, name, loc = 'cyt', new_value = 0):
+		return set_initial.monomers(model, name, loc, new_value)
+
+	def pattern(model, name, alias = '', new_value = 0):
+		model = alias_model_components(model)
+		exec('Initial(' + name + ', Parameter(\'t0_' + alias + '\', ' + str(new_value) + '))')
+		return model
 
 def ode(model, start = 0, finish = 10, points = 10, path = '/opt/'):
 	set_path('bng', path)
@@ -33,13 +54,38 @@ def ode(model, start = 0, finish = 10, points = 10, path = '/opt/'):
 
 	return BngSimulator(model, linspace(start, finish, points+1)).run(method = 'ode').dataframe
 
+def modes(sims, n_runs):
+	data = []
+	for i in range(n_runs):
+		data.append(sims.xs(i))
+
+	avrg = 0
+	for i in range(n_runs):
+		avrg += data[i]
+	avrg = avrg / n_runs
+
+	stdv = 0
+	for i in range(n_runs):
+		stdv = (data[i] - avrg)**2
+	stdv = (stdv / (n_runs-1))**0.5
+
+	return {'sims' : data, 'avrg' : avrg, 'stdv' : stdv}
+
 def ssa(model, start = 0, finish = 10, points = 10, n_runs = 20, path = '/opt/'):
 	set_path('bng', path)
 	generate_network(model)
 	generate_equations(model)
 
-	return BngSimulator(model, linspace(start, finish, points)).run(method = 'ssa', n_runs = n_runs).dataframe
+	sims = BngSimulator(model, linspace(start, finish, points+1)).run(method = 'ssa', n_runs = n_runs).dataframe
+	sims = modes(sims, n_runs)
+	return {'sims' : sims['sims'], 'avrg' : sims['avrg'], 'stdv' : sims['stdv']}
 
-def plot(data, observable, loc, *args, **kwargs):
+def kasim(model, start = 0, finish = 10, points = 10, n_runs = 20, path = '/opt/'):
+	set_path('kasim', path)
+	sims = KappaSimulator(model, linspace(start, finish, points+1)).run(n_runs = n_runs).dataframe
+	sims = modes(sims, n_runs)
+	return {'sims' : sims['sims'], 'avrg' : sims['avrg'], 'stdv' : sims['stdv']}
+
+def plot(data, observable, loc = 'cyt', *args, **kwargs):
 	label = kwargs.get('label', None)
-	plt.plot(data.index, data['obs_' + observable + '_' + loc], label = label)
+	plt.plot(data.index, data['obs_' + observable + '_' + loc.lower()], label = label)
