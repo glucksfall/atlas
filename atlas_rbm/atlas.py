b45a7d25a6c6db91ba0b1e05a3b01d2224f8a292
Rodrigo <glucksfall@users.noreply.github.com>
2020-05-03 23:09:49 -0400

    combine two models into one for simulation

--- atlas_rbm/atlas.py
+++ atlas_rbm/atlas.py
@@ -11,3 +11,91 @@ __license__ = 'gpl-3.0'
 
 from .construct_model_from_metabolic_network import *
 from .construct_model_from_interaction_network import *
+
+from pysb import *
+from pysb.core import *
+from pysb.util import alias_model_components
+
+def combine_models(model, new_model):
+	# find monomers in common and uniques
+	monomer_names1 = []
+	monomer_names2 = []
+	for monomer in model.monomers:
+		monomer_names1.append(monomer.name)
+	for monomer in new_model.monomers:
+		monomer_names2.append(monomer.name)
+
+	commons = list(set(monomer_names1).intersection(monomer_names2))
+	uniques = list(set(monomer_names1).symmetric_difference(monomer_names2))
+
+	new_monomers = []
+	for unique in uniques:
+		for monomer in model.monomers:
+			if unique == monomer.name:
+				new_monomers.append(str(monomer))
+		for monomer in new_model.monomers:
+			if unique == monomer.name:
+				new_monomers.append(str(monomer))
+
+	for common in commons:
+		for monomer in model.monomers:
+			if common == monomer.name:
+				a = monomer.site_states['name']
+		for monomer in new_model.monomers:
+			if common == monomer.name:
+				b = monomer.site_states['name']
+
+		new_monomers.append(
+			"Monomer('{:s}', {:s}, {{'name': {:s}, 'loc': {:s}}})".format(
+				str(monomer.name),
+				str(monomer.sites),
+				str(list(set(a+b))),
+				str(monomer.site_states['loc'])))
+
+	new_rules = []
+	for rule in model.rules:
+		new_rules.append(str(rule))
+	for rule in new_model.rules:
+		new_rules.append(str(rule))
+
+	new_parameters = []
+	for parameter in model.parameters:
+		new_parameters.append(str(parameter))
+	for parameter in new_model.parameters:
+		new_parameters.append(str(parameter))
+
+	new_initials = []
+	for initial in model.initials:
+		new_initials.append(str(initial))
+	for initial in new_model.initials:
+		new_initials.append(str(initial))
+
+	new_observables = []
+	for observable in model.observables:
+		new_observables.append(str(observable))
+	for observable in new_model.observables:
+		new_observables.append(str(observable))
+
+	new_model.rules = ComponentSet()
+	new_model.monomers = ComponentSet()
+	new_model.parameters = ComponentSet()
+	new_model.observables = ComponentSet()
+	new_model.initials = []
+
+	for new_monomer in new_monomers:
+		exec(new_monomer)
+
+	for new_parameter in sorted(set(new_parameters)):
+		exec(new_parameter)
+
+	alias_model_components(new_model)
+	for new_initial in sorted(set(new_initials)):
+		exec(new_initial)
+
+	for new_rule in sorted(set(new_rules)):
+		exec(new_rule)
+
+	for new_observable in sorted(set(new_observables)):
+		exec(new_observable)
+
+	return new_model
